3980815e9be7de17c836fb00687be1ceb2fb6891
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2019-05-16 14:14:58 +0600

    added try-catch block with JSON.parse() to prevent spitting up stacktrace

--- controllers/DatabaseController.js
+++ controllers/DatabaseController.js
@@ -19,20 +19,34 @@ var refreshDatabase = function() {
 
 var getALlCourses = function () {
     let records = fs.readFileSync(path.join(__dirname, '../data.json'));
-    return JSON.parse(records);
+    try {
+        records = JSON.parse(records);
+    } catch (err) {
+        return {
+            status: "Failed",
+            message: "Parser failed to get data, try again in a minute"
+        };
+    }
+    return records;
 };
 
 var getByCourseCode = function(courseCode) {
     let records = getALlCourses();
-    records = records.filter(record => record.name.includes(courseCode));
+    
+    if (!records.status) {
+        records = records.filter(record => record.name.includes(courseCode));
+    }
 
     return records;
 };
 
 var getAvailableCourseByTiming = function(timing) {
     timing = parseTime(timing);
-    let records = getALlCourses();    
-    records = records.filter(record => record.time.includes(timing) && record.capacity_remaining > 0);
+    let records = getALlCourses();
+    
+    if (!records.status) {
+        records = records.filter(record => record.time.includes(timing) && record.capacity_remaining > 0);
+    }    
 
     return records;
 };
